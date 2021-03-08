7a72799a16cb1d0212059085fad5bb42bb88e309
Luciano Santillan <lsantillan@digbang.com>
2021-03-08 20:41:35 -0300

    refactor

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin / env node
+
+const { app } = require("./src/app");
+
 // index.js
 
-const { generarSceneClassFile } = require('./SceneClassFileGenerator.js')
-const { generarScssClassFile } = require('./ScssFileGenerator.js')
-const { generarStoreClassFile } = require('./StoreFileGenerator.js')
-const Console = require('./utils/Console.js')
-const {capitalizeFirstLetter} = require('./utils/StringUtils.js')
-const fs = require('fs')
-const { generarIndexJsFile } = require('./indexJsFileGenerator.js')
-
-const rootPath = 'src/scenes'
-
-if (!fs.existsSync(rootPath)) {
-    Console.write(`error: Para poder crear la scena, la ruta ${rootPath} debe existir`)
-}
-Console.write("Escriba el nombre de la scene \n")
-Console.read("data", (data) => {
-    let sceneName = capitalizeFirstLetter(data.toString().trim());
-    if (fs.existsSync(`${rootPath}/${sceneName}`)) {
-        Console.write("Este nombre de escena ya existe, elija otro:");
-        return;
-    }
-    fs.mkdirSync(`${rootPath}/${sceneName}`)
-    generarSceneClassFile(sceneName, rootPath)
-    generarScssClassFile(sceneName, rootPath)
-    generarStoreClassFile(sceneName, rootPath)
-    generarIndexJsFile(sceneName, rootPath)
-})
+app()
